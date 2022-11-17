4abb996628709f64a163b5b13792603951286c00
Felipe Palazzo <palazzofelipe@gmail.com>
2022-11-17 18:35:01 +0000

    add fetch

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,7 +1,15 @@
-export default function IndividualPost() {
+const getPerson = async () => {
+  const res = await fetch("https://swapi.dev/api/people/1/");
+  return res.json();
+};
+
+export default async function IndividualPost() {
+  const person = await getPerson();
+
   return (
     <>
       <h2>My Blog Post</h2>
+      <h3>{person.name}</h3>
       <div>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit
         error nulla, inventore consequuntur labore quod provident praesentium
